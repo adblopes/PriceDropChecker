fdaf412117fedbd8728ab11c7e44406e7533be96
Diogo Lopes <adiogo.blopes@hotmail.com>
2023-10-08 18:53:40 +0200

    removed space

--- PriceDropCheck/HttpRequest.cs
+++ PriceDropCheck/HttpRequest.cs
@@ -54,7 +54,6 @@ namespace PriceDropCheck
                         {
                             return;
                         }
-
                     }
                     else
                     {
